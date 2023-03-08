acd9b416d391bc00d8c3c9d752121d57a9ec7105
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-08 19:42:46 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,129 @@
+import java.util.Random;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+       int razmer = 10;
+        int[] arei = new int[razmer];
+        arei [0]=1;
+        arei [1]=2;
+        arei [2]=3;
+        arei [3]=4;
+        arei [4]=5;
+        arei [5]=6;
+        arei [6]=7;
+        arei [7]=8;
+        arei [8]=9;
+        arei [9]=10;
+        System.out.println();
+       /* Random random = new Random();
+        int size = 10;
+        int[] array = new int[size];
+
+
+        for (int i = 0; i < size; i++) {
+            int aa = random.nextInt(100);
+            array[i] = aa;
+            System.out.println(i);
+
+
+        }*/
+        /*Random random = new Random();
+        int size = 10;
+        int[] array = new int[size];
+
+
+        int kill = 0;
+        while (kill < 10){
+            int ss = random.nextInt(100);
+            array[kill] = ss;
+            kill++;
+            System.out.println(ss);
+        }
+        /*Random random = new Random();
+        int t = 10;
+        int[] p = new int[t];
+        int count = 0;
+        for (int i = p.length ; i <= 20; i++) {
+            int ff  = random.nextInt();
+
+
+            System.out.println(ff);
+        }
+      /* Random random = new Random();
+         int[] kill = new int[10]; 
+
+         int count =0;
+         for (int i =0; i < 10; i++) {
+             int ff = random.nextInt(20);
+                   count+=ff;
+                 kill[i]=ff;
+         }
+        System.out.println(count+"\n"+"арифметикалык сан"+"\t"   +(count) / (double) 10);*/
+       /* Random random =new Random();
+        int[] dd = new int[20];
+        int ruu =0;
+        for (int i = 0; i < 20; i++) {
+            int mur = random.nextInt( 20);
+            ruu+=mur;
+            dd[i] = ruu; 
+
+            System.out.print(mur + "+");
+        }
+        System.out.print( "="+ruu);*/
+       /* Random random =new Random();
+        int[] dd = new int[20];
+        int max = dd[0];
+        for (int i = 0; i < dd.length; i++) {
+            int mur = random.nextInt( 0,20);
+            dd[i]=mur;
+            System.out.println(mur+" ");
+            }
+        for (int j = 1; j < dd.length; j++) {
+          if (dd[j]>max){
+              max=dd[j];
+          }
+        }
+        System.out.println(max); */
+      /* Random random = new Random();
+        int[] mon = new int[20];
+        int max = mon[0];
+        int kill =  random.nextInt();
+        for (int i = 0; i < mon.length; i++) {
+
+            if (mon[i]>max){
+                max=mon[i];
+            }
+
+        }
+        System.out.println(max);*/
+        /*Random random = new Random();
+        int a = random.nextInt();
+        int b = random.nextInt();
+        int c = random.nextInt();
+        char a = random.nextInt().charAt();*/
+
+    }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
